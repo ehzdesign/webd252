9826fc125502f6a6fb9997b70087a45b1f778b6f
91.eahz@gmail.com <EHZ@Ericks-MacBook-Pro.local>
2016-01-19 23:24:13 -0500

    first commit

---
+++ wp-content/themes/basic/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Hello</title>
+  <?php wp_head(); ?>
+  <link rel="stylesheet" href=" <?php echo get_stylesheet_uri(); ?>">
+</head>
+<body>
+</header>
+  
